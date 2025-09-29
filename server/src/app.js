e0d93bca02f83d795b1dc4272bca2018dd194a32
Mitesh Prajapati <mitesh5.mprajapati@gmail.com>
2025-09-29 21:58:36 +0530

    Implement CORS middleware

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,14 @@
 import express from 'express';
+import cors from 'cors';
+
 import planetsRouter from './routes/planets/planets.router.js';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
 
 app.get('/hello', (req, res) => {
     res.json({
